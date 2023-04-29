e82f8b172d93e17544ef1741ba14f1900696a619
Komenpurity <jepkorirpurity8@gmail.com>
2023-04-29 14:14:51 +0300

    added a botcollection and bot army component

---
+++ src/components/BotArmy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BotArmy() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
---
+++ src/components/BotCollection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BotCollection() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
