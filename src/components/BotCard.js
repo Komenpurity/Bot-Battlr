9cb18381363c60cf638e22f9824673567fddbf08
Komenpurity <jepkorirpurity8@gmail.com>
2023-04-29 16:17:43 +0300

    added bot army component to the bot collection

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -2,15 +2,15 @@ import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHeartPulse, faBolt, faShieldHalved} from '@fortawesome/free-solid-svg-icons'
 
-export default function BotCard({id,name,url,catchphrase,created,bclass,damage,health,armor}) {
+export default function BotCard({id,name,url,catchphrase,bclass,damage,health,armor}) {
   return (
-    <div className="card col-2" >   
+    <div key={id} className="card col-2" >    
         <img src={url} className="card-img-top"alt='im'/> 
         <div className="card-body" >
             <h5 className="card-title">{name}
                <small>{bclass} </small>  
             </h5>
-            <p className="meta text-wrap">
+            <p>
               <small>{catchphrase}</small>
             </p> 
           <div>
--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import BotCard from './BotCard'
+import BotArmy from './BotArmy'
 
 export default function BotCollection() {
     const [bot,setBot] = useState([])
@@ -12,12 +13,18 @@ export default function BotCollection() {
 
   return (
     <div className='container'> 
+    
+      <div className='bot-army'>
+          <BotArmy />
+      </div>
+
       <div className='row'> 
           {bot.map((e) => {
               return <BotCard id={e.id} name={e.name}  bclass={e.bot_class} damage={e.damage} health={e.health} armor={e.armor}
               url={e.avatar_url}  catchphrase={e.catchphrase} created={e.created_at} /> 
           })}
       </div>
+
     </div>
   )
 }
