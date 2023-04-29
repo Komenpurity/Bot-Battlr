ca0ee32bac139ed688df435e5edcb69ffb9a038d
Komenpurity <jepkorirpurity8@gmail.com>
2023-04-29 14:24:13 +0300

    added a fetch get request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BotCollection from './components/BotCollection';
 
 function App() {
   return (
     <div className="App">
-      
+      <BotCollection />
     </div>
   );
 }
--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function BotCollection() {
+    
+
+    useEffect(() => {
+        fetch("http://localhost:3000/bots")
+        .then(response => response.json())
+        .then(data => console.log(data))
+    },[])
+
   return (
     <div>
         
